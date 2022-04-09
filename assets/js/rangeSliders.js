e335783a317822bc8fa5f8cc35bfb2e82a754cd7
MiguelAlvesMiguel <alvesmiguelm@gmail.com>
2022-04-09 20:01:31 +0100

    funcionalidade

--- assets/js/rangeSliders.js
+++ assets/js/rangeSliders.js
@@ -1,21 +1,6 @@
-var range = document.getElementById("FirstRange");
-var output = document.getElementById("lblOne");
-output.innerHTML = range.value;
-
-range.oninput = function () {
-    output.innerHTML = this.value;
-}
-var range2 = document.getElementById("SecondRange");
-var output2 = document.getElementById("lblTwo");
-output2.innerHTML = range2.value;
-
-range2.oninput = function () {
-    output2.innerHTML = this.value;
-}
-var range3 = document.getElementById("ThirdRange");
-var output3 = document.getElementById("lblThree");
-output3.innerHTML = range3.value;
-
-range3.oninput = function () {
-    output3.innerHTML = this.value;
-}
+$(document).ready(function(){ 
+    $(".drop-down-wrapper select").on("change",function(){
+        var selVal = $(this).val();
+        $("h2").html(selVal);
+    });
+});
