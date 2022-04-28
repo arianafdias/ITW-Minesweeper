6cbbedae8a2f4d6b0233b39dfeeada38093e4ab7
MiguelAlvesMiguel <alvesmiguelm@gmail.com>
2022-04-28 01:42:39 +0100

    shrug

--- assets/js/minesweeper.js
+++ assets/js/minesweeper.js
@@ -8,4 +8,27 @@ window.onload = function () {
         gridItem.className = 'grid-item';
         gridContainer.appendChild(gridItem);
     }
+    var color = Cookie.get("color");
+
+        var allGridItems = document.getElementsByClassName("grid-item");
+        colorPicker.value = color;
+
+      
+        //Change all grid items border color
+        for (var i = 0; i < allGridItems.length; i++) {
+            allGridItems[i].style.borderColor = color;
+        }
+        var darkerColor=mudarBrightness(color, -55);
+
+        gridContainer[0].style.backgroundColor=darkerColor;
+        var css = '.grid-item:hover{ background-color:'+darkerColor;+'; color: black;}';
+var style = document.createElement('style');
+
+if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+} else {
+    style.appendChild(document.createTextNode(css));
+}
+
+document.getElementsByTagName('head')[0].appendChild(style);
 }
