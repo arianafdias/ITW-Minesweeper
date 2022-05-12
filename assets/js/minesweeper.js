7d7683825763174865b430346e7573a0af4156a2
Diogo <diogobailaoforte@gmail.com>
2022-05-12 09:23:12 +0100

    ojop

--- assets/js/minesweeper.js
+++ assets/js/minesweeper.js
@@ -301,12 +301,9 @@ function gameWon(){
 
 
 
-
+const loginform = document.getElementsByClassName("form1");
+var userdata = {};
 function registerNewUser(){
-    var new_user = $('#new-username').val();
-    var new_pass = $('#new-password').val();
-    var confirm_pass = $('#confirm-password').val();
-    var new_email = $('#new-email').val();
     $.post('createaccount.php', {
         new_user: new_user,
         new_pass: new_pass,
