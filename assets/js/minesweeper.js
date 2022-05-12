a048233361ac4b3dbc9aee84968065b395fab600
Diogo <diogobailaoforte@gmail.com>
2022-05-12 08:39:50 +0100

    verve

--- assets/js/minesweeper.js
+++ assets/js/minesweeper.js
@@ -298,3 +298,25 @@ function gameWon(){
     //addscore()
     window.location.href = "scoreindivid.html";    
 };
+
+
+
+
+function registerNewUser(){
+    var new_user = $('#new-username').val();
+    var new_pass = $('#new-password').val();
+    var confirm_pass = $('#confirm-password').val();
+    var new_email = $('#new-email').val();
+    $.post('createaccount.php', {
+        new_user: new_user,
+        new_pass: new_pass,
+        confirm_pass: confirm_pass,
+        new_email: new_email
+    });
+};
+
+function Login(user,pass){
+     $.post("demo_test.asp", function(data, status){
+          alert("Data: " + data + "\nStatus: " + status);
+        })
+}
