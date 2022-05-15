d1fadeff535e5319ebf8172cb3f7ac3fe563eefe
Diogo <diogobailaoforte@gmail.com>
2022-05-15 17:25:02 +0100

    scoreboard dev

--- assets/js/score.js
+++ assets/js/score.js
@@ -6,6 +6,7 @@ window.onload = function () {
         colorPicker.value = Cookie.get("color");
     }
     changeColour();
+    scoretable();
 
 
 }
@@ -24,3 +25,34 @@ function changeColour() { //Tem que tar dentro da função para mudar tudo em te
         var colorPicker= document.getElementById("colorPicker");
         Cookie.set("color", colorPicker.value, 365);
     }   
+
+//user array position (tem todos os dados dos jogos que o jogador fez no localstorage)
+
+
+
+
+
+function scoretable(){
+    let deftabela = document.getElementById("scoreboard");
+    let newtabela = document.createElement("table")
+    newtabela.setAttribute("id", "scoreboard");
+    let tabline = document.createElement("tr"); 
+    tabline.appendChild(linhaTabela);
+    tabline.innerHTML = "<th>Jogador</th>" + "<th>Pontos</th>"+"<th>Tempo</th>";
+    newtabela.append(tabline);
+
+
+    let numdata = 0;
+    for (let scoreline in gamedata){
+        while(numdata < 10){
+        tabline = document.createElement("tr");
+        tabline.innerHTML = "<td>" + //gamedata.playername + "</td>" +
+                                "<td>" + //gamedata.playertime + "</td>" +
+                                "<td>" + //gamedata.playerscore + "</td>";
+        newtabela.appendChild(tabline);   
+        }    
+    numdata++;   
+    }   
+    deftabela.parentNode.replaceChild(newtabela, deftabela);
+
+}
